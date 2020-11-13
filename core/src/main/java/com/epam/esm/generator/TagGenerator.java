a55124f74350da712396e15f3edf2e73dd1a038c
VadimKominch <kominch.vadim@yandex.ru>
2020-11-13 08:09:27 +0300

    add entities generator

---
+++ core/src/main/java/com/epam/esm/generator/TagGenerator.java
@@ -0,0 +1,13 @@
+package com.epam.esm.generator;
+
+import com.epam.esm.entity.Tag;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TagGenerator {
+    public static List<Tag> getTags(int count) {
+        return IntStream.range(0,count).mapToObj(el->new Tag("Tag №"+el,0)).collect(Collectors.toList());
+    }
+}
