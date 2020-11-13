092e456ad67fdae96da7f5417f1d7e996358e3d1
VadimKominch <kominch.vadim@yandex.ru>
2020-11-13 08:03:53 +0300

    add new entity User

--- core/src/main/java/com/epam/esm/entity/User.java
+++ core/src/main/java/com/epam/esm/entity/User.java
@@ -1,4 +1,78 @@
 package com.epam.esm.entity;
 
-public class User {
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class User extends RepresentationModel<User> {
+
+    private int id;
+    private String name;
+    private String surname;
+
+    private List<GiftSertificate> certificates;
+
+    public User(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.certificates = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public List<GiftSertificate> getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(List<GiftSertificate> certificates) {
+        this.certificates = certificates;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(surname, user.surname) &&
+                Objects.equals(certificates, user.certificates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, certificates);
+    }
 }
