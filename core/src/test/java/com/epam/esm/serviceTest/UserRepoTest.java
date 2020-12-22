a167495831e105c2af0d192f6737b4b132030881
VadimKominch <kominch.vadim@yandex.ru>
2020-12-22 20:33:59 +0300

    add test to user service layer

---
+++ core/src/test/java/com/epam/esm/serviceTest/UserRepoTest.java
@@ -0,0 +1,40 @@
+package com.epam.esm.serviceTest;
+
+import com.epam.esm.TestConfig;
+import com.epam.esm.entity.User;
+import com.epam.esm.generator.UserGenerator;
+import com.epam.esm.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TestConfig.class})
+@ActiveProfiles("test")
+public class UserRepoTest {
+    @Autowired
+    private UserService service;
+
+    @Before
+    public void setUp() {
+        List<User> users = UserGenerator.getUsers(100);
+        users.forEach(el->service.save(el));
+    }
+
+    @Test
+    public void check() {
+        Assert.assertEquals(100,service.getAll().size());
+    }
+
+    @Test
+    public void checkIfNull() {
+
+    }
+}
