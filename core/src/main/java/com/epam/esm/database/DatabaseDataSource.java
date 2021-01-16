035f5ba7bbf30932c7c1eff3808bad636bb7ab69
VadimKominch <kominch.vadim@yandex.ru>
2021-01-17 00:11:14 +0300

    update command line runner to save all entities from task

--- core/src/main/java/com/epam/esm/database/DatabaseDataSource.java
+++ core/src/main/java/com/epam/esm/database/DatabaseDataSource.java
@@ -2,20 +2,30 @@ package com.epam.esm.database;
 
 import com.epam.esm.dao.GiftDao;
 import com.epam.esm.dao.TagDao;
+import com.epam.esm.entity.GiftSertificate;
 import com.epam.esm.entity.Tag;
+import com.epam.esm.entity.User;
+import com.epam.esm.generator.CertificateGenerator;
 import com.epam.esm.generator.TagGenerator;
+import com.epam.esm.generator.UserGenerator;
 import com.epam.esm.service.GiftService;
 import com.epam.esm.service.TagService;
+import com.epam.esm.service.UserService;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Configuration class for datasource beans: developer and test
@@ -51,8 +61,31 @@ public class DatabaseDataSource {
     }
 
     @Bean
-    CommandLineRunner initDatabase(GiftService certificateRepository, TagService tagRepository) {
+    @Transactional
+    CommandLineRunner initDatabase(GiftService certificateRepository, TagService tagRepository, UserService repository) {
         List<Tag> tags = TagGenerator.getTags(1000);
-        return args -> tagRepository.saveAll(tags);
+        List<GiftSertificate> certificates = CertificateGenerator.getTags(1000);
+        List<User> users = UserGenerator.getUsers(1000);
+
+        return args -> {
+            repository.saveAll(users);
+            tagRepository.saveAll(tags);
+            for(int i =1;i<1001;i++) {
+                GiftSertificate giftCertificate = certificates.get(i-1);
+                User savedUSer = users.get(i-1);
+                savedUSer.setId(i);
+                giftCertificate.setUser(savedUSer);
+                int startIndex = ThreadLocalRandom.current().nextInt(1, 1000);
+                List<Tag> tagsSet = new ArrayList<>();
+                Tag tag1 = tags.get(startIndex-1);
+                Tag tag2 = tags.get(startIndex);
+                tag1.setId(startIndex);
+                tag2.setId(startIndex+1);
+                tagsSet.add(tag1);
+                tagsSet.add(tag2);
+                giftCertificate.setTags(tagsSet);
+                certificateRepository.save(giftCertificate);
+            }
+        };
     }
 }
