5e8d23ae901a7970c0fa53f320a58f6848344209
VadimKominch <kominch.vadim@yandex.ru>
2021-01-17 00:10:42 +0300

    add certificate generator for command runner

--- core/src/main/java/com/epam/esm/generator/CertificateGenerator.java
+++ core/src/main/java/com/epam/esm/generator/CertificateGenerator.java
@@ -1,4 +1,24 @@
 package com.epam.esm.generator;
 
+import com.epam.esm.entity.GiftSertificate;
+import com.epam.esm.entity.Tag;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class CertificateGenerator {
+    public static List<GiftSertificate> getTags(int count) {
+        return IntStream.range(0,count).mapToObj(el->{
+            GiftSertificate certificate = new GiftSertificate();
+            certificate.setName("Certificate №"+el);
+            certificate.setDescription("Description for certificate №"+el);
+            certificate.setPrice(Math.round(Math.random()*30)+10);
+            certificate.setDuration((short) (Math.round(Math.random()*30)+10));
+            certificate.setLastUpdateDate(new Date());
+            certificate.setCreationDate(new Date());
+            return certificate;
+        }).collect(Collectors.toList());
+    }
 }
