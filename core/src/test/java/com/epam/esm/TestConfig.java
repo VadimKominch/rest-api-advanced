190c44d02ffa3b75c70e982fd09bd0adbd8b26ec
VadimKominch <kominch.vadim@yandex.ru>
2021-01-19 15:09:00 +0300

    update test config with environment

--- core/src/test/java/com/epam/esm/TestConfig.java
+++ core/src/test/java/com/epam/esm/TestConfig.java
@@ -1,7 +1,36 @@
 package com.epam.esm;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = "com.epam.esm")
 public class TestConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    @Profile("test")
+    public LocalSessionFactoryBean getSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        Properties properties = new Properties();
+        System.out.println(env.getProperty("hibernate.show_sql"));
+        System.out.println(env.getProperty("hibernate.ddl_auto"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto",env.getProperty("hibernate.ddl_auto"));
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.setAnnotatedClasses(Tag.class, GiftCertificate.class, User.class);
+        return factoryBean;
+    }
 }
